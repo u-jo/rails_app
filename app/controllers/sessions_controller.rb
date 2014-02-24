2083031fc12239d60fe78b641d887011b40e5232
u-jo <lee.yuzhou@gmail.com>
2014-02-23 22:30:39 -0500

    created form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
       		# Sign the user in and redirect to the user's show page.
     		sign_in user
     		redirect_to user
